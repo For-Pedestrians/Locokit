6ed7ccb5982585995710deeb2e4acc78bba2d260
Eric Ngo <ericn441@gmail.com>
2024-05-30 13:14:28 -0600

    make sure saves save

--- LocoKit/Timelines/TimelineObjects/PersistentSample.swift
+++ LocoKit/Timelines/TimelineObjects/PersistentSample.swift
@@ -26,6 +26,10 @@ open class PersistentSample: LocomotionSample, TimelineObject {
         timelineItem?.invalidate()
     }
 
+    public override var secondsFromGMT: Int? {
+        didSet { if oldValue != secondsFromGMT { hasChanges = true } }
+    }
+
     internal override var _classifiedType: ActivityTypeName? {
         didSet { if oldValue != _classifiedType { hasChanges = true } }
     }
